770ef37430f4328a3ac98b527ff02de1fc282ee1
FruHafizd <muhammadhafidz12333@gmail.com>
2024-11-12 20:40:33 +0800

    Deploy Again

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ import forms from '@tailwindcss/forms';
 export default {
 
     content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
