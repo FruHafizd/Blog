11933c016f81d211de48819ce91a99bb0d104c9a
FruHafizd <muhammadhafidz12333@gmail.com>
2024-11-12 20:47:20 +0800

    Deploy Again

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite('resources/css/app.css')
+    @vite(['resources/css/app.css', 'resources/js/app.js']) <!-- Pastikan baris ini ada -->
     <script src="https://cdn.jsdelivr.net/npm/preline/dist/preline.js"></script>
     <script src="//unpkg.com/alpinejs" defer></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.css">
